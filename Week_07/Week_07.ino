d52e2fad84cf374b907791da2998fc2840bf9732
Leah Takele <leahtakele@gmail.com>
2016-10-13 18:40:03 -0400

    arduino homework 3

---
+++ Week_07/Week_07.ino
@@ -0,0 +1,19 @@
+int led = 13;
+//int button = 12;
+void setup() {
+pinMode(led, OUTPUT);
+//pinMode(button, INPUT);
+Serial.begin(9600);
+}
+void loop(){
+if(Serial.available() > 0) {
+char ledState = Serial.read();
+if(ledState == '1'){
+digitalWrite(led, HIGH);
+}
+if(ledState == '0'){
+digitalWrite(led, LOW);
+}
+}
+
+}
